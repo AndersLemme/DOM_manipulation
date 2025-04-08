3eb1deb7beb16de9bd7b2ca146a48d3f2f1454de
AndersLemme <a.lemme@hotmail.com>
2025-04-08 22:13:19 +0200

    Added dynamic red text

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
+const red = document.createElement("p");
+red.classList.add("red");
+red.style.color = "red"
+red.textContent = "Hey, i'm red!";
+
 container.appendChild(content);
+container.appendChild(red);
+
